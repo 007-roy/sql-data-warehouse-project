0ec5a4a0284bda98843ca863aff3727902afeacb
Satyen Roy Chowdhury <81115135+007-roy@users.noreply.github.com>
2026-04-25 09:59:25 +0600

    Update init_databse.sql

--- scripts/init_databse.sql
+++ scripts/init_databse.sql
@@ -1,1 +1,34 @@
 
+
+USE master;
+GO
+
+
+--Drop and recreate the 'DataWarehouse' Database
+IF EXISTS (SELECT ! FROM sys.databases WHERE name='DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+
+-- CREATE DataWarehouse Database
+CREATE DATABASE DataWarehouse;
+GO
+
+
+USE DataWarehouse;
+
+
+-- CREATE SCHEMAS
+CREATE SCHEMA bronze;
+GO
+
+
+CREATE SCHEMA silver;
+GO
+
+
+CREATE SCHEMA gold;
+GO
